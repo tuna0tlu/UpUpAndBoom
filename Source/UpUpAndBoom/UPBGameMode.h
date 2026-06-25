f23d98b5f55a77df026ef257d1157adb1e2c42f1
TunisMan <131547316+TunisMan@users.noreply.github.com>
2026-06-25 20:16:35 +0300

    Initial commit

---
+++ Source/UpUpAndBoom/UPBGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UPBGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UPUPANDBOOM_API AUPBGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
